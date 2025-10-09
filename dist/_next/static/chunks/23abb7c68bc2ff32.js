0ed58bcb8cb9fdf7a0de2dff13e7a86a1293e9f5
Rohith Kumar Gujarathi <118886530+RohithReacts@users.noreply.github.com>
2025-10-09 10:07:03 +0530

    delete

--- dist/_next/static/chunks/23abb7c68bc2ff32.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/68eacf808f8c396d.js",
-  "static/chunks/fce09a736382aabd.js",
-  "static/chunks/81d4479dfdd83bb3.js",
-  "static/chunks/turbopack-6d51fd8fc3274b10.js"
-])
--- dist/_next/static/chunks/51c36071ffd080ee.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/luggageproducts", [
-  "static/chunks/035c58d97c285f97.js",
-  "static/chunks/81d4479dfdd83bb3.js",
-  "static/chunks/fce09a736382aabd.js",
-  "static/chunks/a551ebb9631642d8.js",
-  "static/chunks/a133f878e23c1b2f.js",
-  "static/chunks/turbopack-1635d05af84bacaf.js"
-])
