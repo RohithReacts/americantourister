d49b204dbc862663484cdd3dbe2f65b09bb3cd08
Rohith Kumar Gujarathi <118886530+RohithReacts@users.noreply.github.com>
2025-10-09 17:08:00 +0530

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
-  distDir:'dist',
-  images:{
-unoptimized:true,
-  },
+  
+  
   devIndicators: false,
   reactStrictMode: true,
-   // 👇 Important: set basePath if you are deploying under a repo name
-  basePath: '/americantourister',
-  assetPrefix: '/americantourister/',
+ 
 };
 
 export default nextConfig;
